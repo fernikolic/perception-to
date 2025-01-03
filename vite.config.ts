f8c1d4ca7dacebc638be42119d988df023991530
Fernando Nikolic <fernandonikolic@Fernandos-MacBook-Pro.local>
2025-01-03 15:49:24 +0100

    Yet another commit

--- vite.config.ts
+++ vite.config.ts
@@ -21,5 +21,17 @@ export default defineConfig({
     hmr: {
       overlay: false
     }
-  }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
